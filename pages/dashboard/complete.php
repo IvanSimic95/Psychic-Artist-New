53f003b5ef4d791ade8dca0797d68f063b5f5bff
IvanSimic95 <email@isimic.com>
2022-02-09 13:30:21 +0100

    dsa

---
+++ pages/dashboard/complete.php
@@ -0,0 +1,39 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/templates/noskip.php';
+
+$title = "Future Baby Portrait | Last Chance!"; 
+$title2 = "LAST CHANCE TO GRAB IT!";
+$sdescription = "Customize your order";
+$description = "Hey ".$_SESSION['orderFName']."! Here's a last chance to grab your future baby portrait with huge discount & express delivery!";
+?>
+<div class="container-fluid" data-layout="container" style="padding:0!important;padding-top:20px!important;">
+    <section class="py-0 light" id="banner">
+        <div class="container p-0 p-xl-4">
+
+          <div class="row g-0">
+       
+            <div class="col-12 offset-0 col-xl-8 offset-xl-2">
+              <div class="card mb-3">
+                <div class="card-header bg-light" style="text-align:center;">
+                <h3 class="gradient  mb-0">Order complete! </h3>
+                </div>
+                  <div class="card-body col-12 offset-0 col-xl-10 offset-xl-1" style="text-align:center;">
+                You will receive login & order details in your email!
+                 
+                  </div>
+              </div>
+            </div>
+        
+      
+          </div>
+          
+          </div>
+    </section>
+</div>
+
+<?php
+//$customCSSPreload = '<link rel="preload" href="/assets/css/baby.css" as="style">';
+//$customCSS = '<link href="/assets/css/baby.css" rel="stylesheet">';
+//$customJS = <<<EOT
+//EOT;
+?>
