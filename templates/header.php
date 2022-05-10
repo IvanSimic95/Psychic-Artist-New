6ccc319d3e40f9ccff1742392e7ae5c8e56c08ed
IvanSimic95 <email@isimic.com>
2022-05-10 23:02:59 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -68,20 +68,16 @@
     <!-- ===============================================-->
     <!--FBMETATAGS-->
     
- 
- <!-- Hotjar Tracking Code for psychic-artist.com
-
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143014473-9"></script>
 <script>
-(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2564466,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-143014473-9');
 </script>
- -->
+
 
  <script>
 (function(t,a,l,k,j,s){
