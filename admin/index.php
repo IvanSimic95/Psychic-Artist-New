1d40f9bac4462f7be2ba4af7420e68ac95c13471
IvanSimic95 <email@isimic.com>
2022-05-28 21:24:49 +0200

    .

--- admin/index.php
+++ admin/index.php
@@ -31,7 +31,22 @@ $sql3 = "SELECT SUM(order_price) AS sum_quantity FROM orders WHERE (order_status
 $r3 = $conn->query($sql3);
 $fetch3 = $r3->fetch_assoc();
 $sum = $fetch3['sum_quantity'];
+if($sum > 0){
+$sum = round($sum, 2);
+}else{
+$sum = 0;
+}
+$sql4 = "SELECT * FROM orders WHERE order_status = 'processing'";
+$result4 = $conn->query($sql4);
+$countProcessing = mysqli_num_rows($result4);
 
+$sql5 = "SELECT * FROM orders WHERE order_status = 'pending'";
+$result5 = $conn->query($sql5);
+$countPending = mysqli_num_rows($result5);
+
+$sql6 = "SELECT * FROM orders_log WHERE notice = 'Order Status updated to Completed!' AND DATE(time) = '$today'";
+$result6 = $conn->query($sql6);
+$countCompletedToday = mysqli_num_rows($result6);
 ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/admin/templates/auth.php'; ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/admin/templates/head.php'; ?>
@@ -70,10 +85,9 @@ $sum = $fetch3['sum_quantity'];
                             <div class="col">
                               <div class="d-flex">
                                 <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2"></div> Font Awesome fontawesome.com -->
-                                <p class="fs--1 ps-2 mb-0"><strong>5 products</strong> didn’t publish to your Facebook page</p>
+                                <p class="fs--1 ps-2 mb-0"><strong><?php echo $countProcessing; ?> orders</strong> are still processing</p>
                               </div>
                             </div>
-                            <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View products<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
                           </div>
                         </li>
                         <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top border-x-0 border-top-0">
@@ -81,10 +95,9 @@ $sum = $fetch3['sum_quantity'];
                             <div class="col">
                               <div class="d-flex">
                                 <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2 text-primary"></div> Font Awesome fontawesome.com -->
-                                <p class="fs--1 ps-2 mb-0"><strong>7 orders</strong> have payments that need to be captured</p>
+                                <p class="fs--1 ps-2 mb-0"><strong><?php echo $countPending; ?> orders</strong> are waiting for payment</p>
                               </div>
                             </div>
-                            <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View payments<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
                           </div>
                         </li>
                         <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top  border-0">
@@ -92,10 +105,9 @@ $sum = $fetch3['sum_quantity'];
                             <div class="col">
                               <div class="d-flex">
                                 <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2 text-primary"></div> Font Awesome fontawesome.com -->
-                                <p class="fs--1 ps-2 mb-0"><strong>50+ orders</strong> need to be fulfilled</p>
+                                <p class="fs--1 ps-2 mb-0"><strong><?php echo $countCompletedToday; ?> orders</strong> completed today</p>
                               </div>
                             </div>
-                            <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View orders<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
                           </div>
                         </li>
                       </ul>
